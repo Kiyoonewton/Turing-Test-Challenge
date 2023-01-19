90ff9b2edb9a307fcf206f62426f85f864843651
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-19 11:59:02 +0100

    Print out a number power

--- powerSet.js
+++ powerSet.js
@@ -1,9 +1,5 @@
 const Subsets = (nums) => {
   const result = [];
-
-  //   let n = nums.length;
-  //   let powSize = Math.pow(2, n);
-
   for (let i = 0; i < 2 ** nums.length; i++) {
     const val = [];
     for (let j = 0; j < nums.length; j++) {
@@ -17,3 +13,5 @@ const Subsets = (nums) => {
 };
 
 console.log(Subsets([1, 2, 3]));
+
+console.log(Math.pow(2, 4));
