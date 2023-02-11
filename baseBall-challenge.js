67ba4685f34a3c6b240feeb29a2a2b93e4af8981
Charles Abasimfon <devabascharles@gmail.com>
2023-02-11 11:32:07 +0100

    AdjustMent in calculation

--- baseBall-challenge.js
+++ baseBall-challenge.js
@@ -1,12 +1,11 @@
-input_ops = ["5", "2", "4", "C", "D", "+"];
-
+input_ops = ["5", "-2", "4", "C", "D", "9", "+", "+"];
 const chaFunc = () => {
   let X = [];
   for (let i = 0; i < input_ops.length; i++) {
     if (input_ops[i] === "C") {
       X.pop();
     } else if (input_ops[i] === "D") {
-      X.push(X.reduce((a, b) => a + b));
+      X.push(X[X.length - 1] * 2);
     } else if (input_ops[i] === "+") {
       X.push(Number(X[X.length - 1] + X[X.length - 2]));
     } else {
